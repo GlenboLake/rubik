83fb05d9fd8090895e13e8dcda2ece71164a11d5
Glen Haber <glenbolake@gmail.com>
2017-12-04 08:48:48 -0500

    Consider all F2l pair orders

--- solve/f2l.py
+++ solve/f2l.py
@@ -1,12 +1,16 @@
 from collections import namedtuple
+from itertools import permutations
 
 from cube import Cube, Side
+from solve import simplify_algorithm
 from solve.f2l_cases import get_case_solution
 
 _F2LPair = namedtuple('_F2LPair', 'edge,corner')
 
 
 class F2LSolver(object):
+    _slots = ((Side.L, Side.F), (Side.R, Side.F), (Side.L, Side.B), (Side.R, Side.B))
+
     def __init__(self, cube):
         self.cube = cube.copy()
         corners = {c for c in self.cube.cubies if len(c.faces) == 3 and Side.D in c.faces.values()}
@@ -14,27 +18,36 @@ class F2LSolver(object):
                  len(c.faces) == 2 and Side.D not in c.faces.values() and Side.U not in c.faces.values()}
         self.cube.cubies = corners | edges
 
-    def _get_pairs(self):
-        pairs = ((Side.L, Side.F), (Side.R, Side.F), (Side.L, Side.B), (Side.R, Side.B))
-        for pair in pairs:
-            edge, corner = sorted(filter(lambda cubie: set(pair) <= set(cubie.faces.values()), self.cube.cubies),
+    def _unsolved_slots(self):
+        return {frozenset(pair.edge.faces.values()) for pair in self._get_pairs() if get_case_solution(pair) != []}
+
+    def _get_pairs(self, cube=None, slots=None):
+        if cube is None:
+            cube = self.cube
+        for pair in slots or self._slots:
+            edge, corner = sorted(filter(lambda cubie: set(pair) <= set(cubie.faces.values()), cube.cubies),
                                   key=lambda cubie: len(cubie.faces))
             yield _F2LPair(edge, corner)
 
     def solve(self):
-        # print(self.cube.cubies)
-        solution = []
-        skips = 1
-        while skips:
-            skips = 0
-            for pair in self._get_pairs():
+        unsolved = self._unsolved_slots()
+        if not unsolved:
+            return []
+        best_seq, best_htm = None, 100  # Worst case scenario is actually ~HTM11 for each pair, so we'll never see 100
+        for order in permutations(unsolved):
+            cube = self.cube.copy()
+            solution = []
+            for pair in self._get_pairs(cube, order):
                 pair_solution = get_case_solution(pair)
                 if pair_solution is None:
-                    skips += 1
-                else:
-                    self.cube.do(pair_solution)
-                    solution.extend(pair_solution)
-        return solution
+                    solution = []
+                    break
+                cube.do(pair_solution)
+                solution.extend(pair_solution)
+            solution = simplify_algorithm(solution)
+            if solution and len(solution) < best_htm:
+                best_seq, best_htm = solution, len(solution)
+        return best_seq
 
 
 if __name__ == '__main__':
@@ -50,8 +63,5 @@ if __name__ == '__main__':
         return Cube(setup)
 
 
-    my_cube = make_cube()
-    f2l_solution = F2LSolver(my_cube).solve()
-    print(' '.join(str(step) for step in f2l_solution))
-    my_cube.do(f2l_solution)
-    my_cube.ascii()
+    solution = F2LSolver(make_cube()).solve()
+    print(' '.join(s.name for s in solution))
